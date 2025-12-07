98c21565f0e495ae8b66a5ffd16aa98c0ece62bb
EXTOY3rd-Nin <tapanadah.1@gmail.com>
2025-12-07 23:23:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+import os
 import yfinance as yf
 import requests
-import os
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # อนุญาตให้เว็บภายนอกเรียก API ได้
